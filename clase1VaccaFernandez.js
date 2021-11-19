47b4657b2b0091aaa580f3afc416d35e1d8ffa2d
lucas fernandez <lucasvf4379@gmail.com>
2021-11-19 20:01:28 -0300

    clase 1

---
+++ clase1VaccaFernandez.js
@@ -0,0 +1,12 @@
+let userName = prompt('Ingrese su nombre: ')
+let userLastName= prompt('Ingrese su apellido:')
+
+let userEmail = prompt('Ingrese su email:')
+let userPhone = prompt('Ingrese su numero:')
+
+alert (`Bienvenido ${userName} ${userLastName}
+        Email: ${userEmail}
+        Telefono: ${userPhone} `  )
+
+let numero_suma = prompt('Ingrese su año de nacimiento:')
+alert (`Su edad es: ${2021 - numero_suma} años`)        
